6a09a606886d5ad32cbfaf000b6220b1be3ba216
charlesstewart <tomdr1001@outlook.com>
2015-12-28 09:57:02 +0000

    Initial commit

---
+++ ConsolePlus/ColouredText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsolePlus
+{
+    public class ColouredText
+    {
+        public static ColouredText NewColouredText(string contentText, ConsoleColor colour)
+        {
+            return new ColouredText {Colour = colour, ContentText = contentText};
+        }
+
+        public static ColouredText NewPlainText(string contentText)
+        {
+            return NewColouredText(contentText, ConsoleColor.White);
+        }
+
+
+        public string ContentText { get; set; }
+        public ConsoleColor Colour { get; set; }
+
+        public override string ToString()
+        {
+            return ContentText;
+        }
+    }
+
+}
---
+++ ConsolePlus/ConsoleManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace ConsolePlus
+{
+    public class ConsoleManager
+    {
+        public static void TypeTextFromFile(string filename, ConsoleColor colour, TypeSpeed typeSpeed)
+        {
+            Type(ReadTextFromFile(filename, colour), typeSpeed);
+        }
+        public static void TypeText(ColouredText text, TypeSpeed typeSpeed)
+        {
+            Console.ForegroundColor = text.Colour;
+
+            var random = new Random();
+
+            foreach (var letter in text.ContentText)
+            {
+                if (letter == '|')
+                {
+                    Console.Write("\n");
+                }
+                else
+                {
+                    Console.Write(letter);
+                }
+
+                Thread.Sleep(random.Next(1, (int)typeSpeed) * 10);
+            }
+
+            Console.Write(" ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void Type(ColouredText[] textCol, TypeSpeed typeSpeed)
+        {
+            foreach (var text in textCol)
+            {
+                TypeText(text, typeSpeed);
+            }
+        }
+
+        public static void WriteTextFromFile(string filename, ConsoleColor colour)
+        {
+            Write(ReadTextFromFile(filename, colour));
+        }
+        public static void WriteText(ColouredText text)
+        {
+            Console.ForegroundColor = text.Colour;
+
+            if (text.ContentText.Contains("|"))
+            {
+                text.ContentText = text.ContentText.Replace("|", "\n");
+            }
+
+            Console.Write(text + " ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void Write(ColouredText[] textCol)
+        {
+            foreach (var text in textCol)
+            {
+                WriteText(text);
+            }
+        }
+
+        private static ColouredText[] ReadTextFromFile(string fileName, ConsoleColor colour)
+        {
+            var reader = new StreamReader(fileName);
+            return reader.ReadToEnd().Split(' ').Select(text => ColouredText.NewColouredText(text, colour)).ToArray();
+        }
+    }
+
+    public enum TypeSpeed
+    {
+        Fast = 15,
+        RatherFast = 30,
+        Medium = 45,
+        RatherSlow = 65,
+        Slow = 100
+    }
+}
